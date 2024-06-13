ba341c226d5eb55b714dea0df8b908a3844b9518
Matthew <castoreno_05@Castoreno.local>
2024-06-12 21:48:53 -0500

    connection.js and gitignore updated.

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,10 +2,18 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("Database connection failed: ", err.stack);
+        return;
+    }
+    console.log("Connected to database.");
 });
 
 module.exports = { db };
